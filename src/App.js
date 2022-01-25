7c90c4c178ec9b531c9819e9cb22465f0eaf1382
drew-fo <drew.ridley.dl@gmail.com>
2022-01-25 13:03:12 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+
 import Title from "./components/Title";
 
 function App() {
@@ -8,21 +8,8 @@ function App() {
       <main>
         <Title />
       </main>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <p>React is cool motherfucker!</p>
     </div>
   );
 }
